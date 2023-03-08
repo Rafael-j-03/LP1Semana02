2cefd5e876eaca0d11c5481e7116b5fc08771267
Rafael-j-03 <ra.david@hotmail.com>
2023-03-08 22:01:14 +0000

    Exercise 9
    Add answer to some questions

--- ChatGPTMother/Program.cs
+++ ChatGPTMother/Program.cs
@@ -10,6 +10,17 @@ namespace ChatGPTMother
             {
                 Console.Write("Place your question: ");
                 string input = Console.ReadLine();
+
+                string response = input switch
+                {
+                    "How are you?" => "I'm fine, thank you!",
+                    "What's your name?" => "Mother",
+                    "What's your mission?" => "Make the world better!",
+                    "What day is it?" => "One day more than yesterday",
+                    _ => "You got me, I don't know what to answer!",
+                };
+
+                Console.WriteLine(response);
             }
         }
     }
