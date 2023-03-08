3f34e3c8af5ffd6e226f8649ef302b2fce373e8f
Rafael-j-03 <ra.david@hotmail.com>
2023-03-08 14:22:38 +0000

    Exercise 4
    Add input to the cylinder's height and radius

---
+++ PerguntaValores/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PerguntaValores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the cylinder's height:");
+            string aQ = Console.ReadLine();
+
+            Console.WriteLine("Please enter the cylinder's radius:");
+            string rQ = Console.ReadLine();
+
+            int a = Convert.ToInt32(aQ);
+
+            int r = Convert.ToInt32(rQ);
+
+            float pi = 3.1415926f;
+
+            float V = pi * r * r * a;
+
+            Console.WriteLine($"The cylinder's volume is: {V}");
+        }
+    }
+}
