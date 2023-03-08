17eb51f6da8c0aeff47c08ca66711aba56b01398
Rafael-j-03 <ra.david@hotmail.com>
2023-03-08 22:19:50 +0000

    Exercise 10
    Print a triangle using the character read, no. of lines equal to the integer read

--- Triangle/Program.cs
+++ Triangle/Program.cs
@@ -6,7 +6,17 @@ namespace Triangle
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n = int.Parse(args[0]);
+            string s = args[1];
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int t = 1; t <= i; t++)
+                {
+                    Console.Write(s);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
